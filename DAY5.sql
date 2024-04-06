3aa19364d88c5b1266160141b968498ec3c73a51
Hanguyen8112 <165490541+Hanguyen8112@users.noreply.github.com>
2024-04-06 21:18:59 +0700

    Update DAY5.sql

--- DAY5.sql
+++ DAY5.sql
@@ -28,4 +28,48 @@ User_id,
 FROM posts
 where post_date >= '01/01/2021' and post_date < '01/01/2022'
 GROUP BY User_id
----
+---EX7
+SELECT
+Card_name,
+MAX(issued_amount)-Min(issued_amount) as difference
+
+FROM monthly_cards_issued
+GROUP BY Card_name
+ORDER BY difference DESC
+---Ex8
+SELECT
+manufacturer,
+COUNT(drug) as drug_count,
+abs(Sum(total_sales - cogs)) as total_loss
+FROM pharmacy_sales
+where total_sales <  cogs
+GROUP BY manufacturer
+ORDER BY Sum(total_sales - cogs) ASC
+--- Ex9
+Select
+id,
+Movie,
+description,
+rating
+From Cinema
+where not description = 'boring'
+and Mod (id,2)=1
+Order by rating desc
+---Ex10
+Select
+Teacher_id,
+Count(distinct(subject_id)) as cnt
+from Teacher
+group by Teacher_id
+---Ex11
+Select
+user_id,
+Count(follower_id) as followers_count
+From Followers
+group by user_id
+---Ex12
+Select
+Class
+From Courses
+Group by class
+Having Count(student) >5
