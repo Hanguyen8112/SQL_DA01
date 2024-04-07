7c7d6b8cf9d15747434ae69f24c17b2154769556
Hanguyen8112 <165490541+Hanguyen8112@users.noreply.github.com>
2024-04-07 12:30:12 +0700

    Update Day7.sql

--- Day7.sql
+++ Day7.sql
@@ -1,1 +1,14 @@
+--Ex1
+Select 
+Name 
+From STUDENTS
+where marks >75
+order by right(name,3) asc, id  asc;
+---Ex2
+Select
+user_id,
+Concat(left(Upper(Name),1),Right(Lower(name),length(name)-1)) as name
+From Users
+Order by User_id
+---Ex3
 
