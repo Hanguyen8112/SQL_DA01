29dcf336d044529a7b2872b78898115aa8980105
Hanguyen8112 <165490541+Hanguyen8112@users.noreply.github.com>
2024-04-08 16:51:19 +0700

    Update Day9.sql

--- Day9.sql
+++ Day9.sql
@@ -21,3 +21,35 @@ Else 'No'
 End as triangle
 from Triangle
 ---Ex3
+SELECT 
+Round(Sum(CASE
+ When call_category = 'n/a' then 1
+ When call_category is null then 1
+ else 0
+end)/Count(policy_holder_id	)*100,1)||'%'
+FROM callers;
+---Ex4
+Select name
+from Customer
+where 
+ Case 
+   when referee_id = 2 then 1
+   else 0
+End = 0
+---Ex5
+select 
+survived,
+sum(case 
+    when pclass = 1 then 1 
+    Else 0
+    end) first_class,
+sum(case 
+    when pclass = 2 then 1 
+    Else 0
+    end) second_class,
+sum(case 
+    when pclass = 3 then 1 
+    Else 0    
+    end) third_class
+from titanic
+group by survived
