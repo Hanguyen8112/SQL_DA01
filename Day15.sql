0cd17861462b549ddff703aa18d5acb3d5a6d13c
Hanguyen8112 <165490541+Hanguyen8112@users.noreply.github.com>
2024-04-22 16:39:34 +0700

    Update Day15.sql

--- Day15.sql
+++ Day15.sql
@@ -37,8 +37,23 @@ FROM
 Rank() over(PARTITION BY user_id ORDER BY transaction_date) as rank
 from transactions) as ranking
 where rank = 3
-
----Ex4
+--Ex4: https://datalemur.com/questions/histogram-users-purchases
+SELECT
+transaction_date,
+user_id,
+purchase_count
+from
+(SELECT 
+ transaction_date,
+ user_id,
+ Count(user_id) as purchase_count,
+ Rank() OVER(PARTITION BY user_id ORDER BY transaction_date desc) as ranking
+FROM user_transactions
+GROUP BY transaction_date,user_id) as latest
+where ranking =1
+ORDER BY transaction_date
+  
+---Ex5: https://datalemur.com/questions/histogram-users-purchases
 with rolling_avg as (SELECT 
 *,
 rank() OVER(PARTITION BY user_id ORDER BY tweet_date) as rank,
@@ -55,3 +70,56 @@ round((case
   else (tweet_count+day2+day1)/3.0
 end),2) as rolling_avg_3d
 from rolling_avg
+
+---Ex6:https://datalemur.com/questions/repeated-payments
+with Minutes as
+(SELECT 
+*,
+Rank() over(PARTITION BY merchant_id,credit_card_id,amount ORDER BY transaction_timestamp) as ranking,
+EXTRACT(EPOCH FROM(transaction_timestamp-Lag(transaction_timestamp) over(PARTITION BY merchant_id,credit_card_id,amount ORDER BY transaction_timestamp)))/60 as diff
+FROM transactions)
+
+SELECT
+Count(transaction_timestamp) as payment_count
+from Minutes
+where
+Ranking >1 and diff >=10
+
+--Ex7: https://datalemur.com/questions/sql-highest-grossing
+with total_spending as (SELECT 
+category,
+product,
+sum(spend) as total_spend,
+RANK() OVER(PARTITION BY category ORDER BY sum(spend) desc) as ranking
+FROM product_spend
+where EXTRACT(year from transaction_date) = 2022
+GROUP BY category,product)
+SELECT
+category,
+product,
+total_spend
+from total_spending
+WHERE ranking <=2
+ORDER BY Category,total_spend DESC
+
+--Ex8
+with cte as 
+(SELECT 
+a.artist_name,
+dense_rank() OVER(ORDER BY Count(s.song_id) desc) as artist_rank
+FROM 
+songs as s
+left join artists as a  
+on s.artist_id=a.artist_id
+left join global_song_rank as g  
+on g.song_id = s.song_id
+where g.rank <11
+GROUP BY a.artist_name)
+
+Select 
+artist_name,
+artist_rank
+from cte
+where artist_rank<6
+
+
