3f0d93a37a86d10a0b97a90b11df08c82076b908
Hanguyen8112 <165490541+Hanguyen8112@users.noreply.github.com>
2024-05-14 23:31:20 +0700

    Update The Look project.sql

--- The Look project.sql
+++ The Look project.sql
@@ -1,9 +1,18 @@
-Select 
+--Ex1
+with cte as (Select 
 Extract(month from created_at) as month,
 Extract(year from created_at) as year,
-Count(distinct(user_id)) as total_user,
-Count(order_id) as total_order
+created_at,
+user_id,
+order_id
 from bigquery-public-data.thelook_ecommerce.orders
-Where delivered_at is not null and '2019-31-08 11:59:59 UTC'<created_at and '2022-01-05 00:00:00 UTC'>created_at
-Group by Extract(month from created_at),Extract(year from created_at)
-order by Year, Month
+Where delivered_at is not null and 
+      created_at< CAST('2022-05-01 00:00:00 UTC' AS TIMESTAMP) and created_at> CAST('2018-12-31 23:59:59 UTC' AS TIMESTAMP)
+order by created_at)
+Select
+ FORMAT_TIMESTAMP('%m-%Y', TIMESTAMP(created_at)) as month_year,
+ Count(distinct(user_id)) as total_user,
+ Count(order_id) as total_order
+From cte
+group by month_year,year, month
+order by year, month
