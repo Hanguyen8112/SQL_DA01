1c899b6375fb3a7d89482db6ef17ffb03cb280bb
Hanguyen8112 <165490541+Hanguyen8112@users.noreply.github.com>
2024-05-15 21:12:25 +0700

    Update The Look project.sql

--- The Look project.sql
+++ The Look project.sql
@@ -5,8 +5,8 @@ Extract(year from created_at) as year,
 created_at,
 user_id,
 order_id
-from bigquery-public-data.thelook_ecommerce.orders
-Where delivered_at is not null and 
+from bigquery-public-data.thelook_ecommerce.order_items
+Where status = 'Complete' and
       created_at< CAST('2022-05-01 00:00:00 UTC' AS TIMESTAMP) and created_at> CAST('2018-12-31 23:59:59 UTC' AS TIMESTAMP)
 order by created_at)
 Select
@@ -16,3 +16,26 @@ Select
 From cte
 group by month_year,year, month
 order by year, month
+
+--Ex2: tổng số lượng khách tăng nhưng giá trị TB/1 đơn ko tăng
+with cte as (Select 
+Extract(month from created_at) as month,
+Extract(year from created_at) as year,
+created_at,
+user_id,
+order_id,
+sale_price
+from bigquery-public-data.thelook_ecommerce.order_items
+Where status = 'Complete' and
+      created_at< CAST('2022-05-01 00:00:00 UTC' AS TIMESTAMP) and created_at> CAST('2018-12-31 23:59:59 UTC' AS TIMESTAMP)
+order by created_at)
+Select
+ FORMAT_TIMESTAMP('%m-%Y', TIMESTAMP(created_at)) as month_year,
+ Count(distinct(user_id)) as total_user,
+ Sum(sale_price)/Count(distinct(order_id)) as Avg_order
+From cte
+group by month_year,year, month
+order by year, month
+
+---Ex3
+
